066d84cb47c65b3e267e2ede1de588e66f9486c2
hamin <ggamini7@gmail.com>
2023-02-13 15:36:43 +0900

    add get accountInfo

--- src/main/java/com/example/bank/controller/AccountController.java
+++ src/main/java/com/example/bank/controller/AccountController.java
@@ -3,11 +3,10 @@ package com.example.bank.controller;
 import com.example.bank.entity.Account;
 import com.example.bank.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AccountController {
@@ -35,13 +34,27 @@ public class AccountController {
 
     @PostMapping("/makeAccount")
     @ResponseBody
-    public String makeAccount(@ModelAttribute Account acc) {
+    public ResponseEntity<String> makeAccount(@ModelAttribute Account acc) {
         try {
             accountService.makeAccount(acc);
-            return "계좌개설 성공";
+            return new ResponseEntity<String>("계좌개설 성공", HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
-            return "계좌개설 실패";
+            return new ResponseEntity<String>("계좌개설 실패", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/accountInfo")
+    @ResponseBody
+    public ResponseEntity<Account> accountInfo(@RequestParam("id") String id) {
+        ResponseEntity<Account> res = null;
+        try {
+            Account acc = accountService.accountInfo(id);
+            res = new ResponseEntity<Account>(acc, HttpStatus.OK);
+        }catch (Exception e) {
+            e.printStackTrace();
+            res = new ResponseEntity<Account>(HttpStatus.BAD_REQUEST);
+        }
+        return res;
     }
 }
--- src/main/java/com/example/bank/service/AccountService.java
+++ src/main/java/com/example/bank/service/AccountService.java
@@ -5,6 +5,8 @@ import com.example.bank.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountService {
 
@@ -13,5 +15,13 @@ public class AccountService {
 
     public void makeAccount(Account acc) throws Exception {
         accountRepository.save(acc);
+    }
+
+    public Account accountInfo(String id) throws Exception {
+        Optional<Account> oacc = accountRepository.findById(id);
+        if (oacc.isEmpty()) {
+            throw new Exception("계좌번호 오류");
+        }
+        return oacc.get();
     }
 }
