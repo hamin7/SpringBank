50983bb4f8e6f63305c00f17e6d4abf5440db269
hamin <ggamini7@gmail.com>
2023-02-13 17:29:38 +0900

    add test code

--- src/test/java/com/example/bank/BankApplicationTests.java
+++ src/test/java/com/example/bank/BankApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.bank;
 
 import com.example.bank.entity.Account;
 import com.example.bank.repository.AccountRepository;
+import com.example.bank.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,18 @@ class BankApplicationTests {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    AccountService accountService;
+
     @Test
     void contextLoads() {
         Account acc = new Account("1000","hong",100000,"special","VIP");
         accountRepository.save(acc);
     }
 
+    @Test
+    void accountInfo() throws Exception {
+        Account acc = accountService.accountInfo("1000");
+        System.out.println(acc);
+    }
 }
